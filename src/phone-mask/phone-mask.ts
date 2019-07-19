715282735ceb89bdf9ea27c09bddf7d7e8473039
Charlles Paiva <charlleshenriquepaiva@hotmail.com>
2019-07-19 17:31:27 -0300

    A simple refactoring

--- src/phone-mask/phone-mask.ts
+++ src/phone-mask/phone-mask.ts
@@ -8,15 +8,14 @@ var VMasker = require('vanilla-masker');
 export class TelefoneMask{
     public nativeElement: HTMLInputElement;
 
-    constructor(public element: ElementRef, public render: Renderer) {
-        console.log(element);
+    constructor(public element: ElementRef, public render: Renderer) { 
         this.nativeElement = this.element.nativeElement;
         
         this.render.listen(this.nativeElement, 'keyup', () => {
-            var s = this.nativeElement.value;
-            var s=s.replace(/[_\W]+/g,'');
-            var n=s.length;
-            if(n<11){var m='(99) 9999-9999?9';}else{var m='(99) 9 9999-9999';}	   
+            const text = (this.nativeElement.value).replace(/[_\W]+/g,'');            
+            
+            const newText = text.length < 11 ? '(99) 9999-9999?9' : '(99) 9 9999-9999';
+            
             VMasker(this.nativeElement).maskPattern(m);
         });
     }
